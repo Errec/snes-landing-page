abe3089bc3fd9116caf3b99076001506a1d5f3b3
Raniro Coelho <errecme@gmail.com>
2017-06-05 21:03:35 -0300

    Add refactor myClassAdmin class argument name and add replaceClass function

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 //Modified from https://jaketrent.com/post/addremove-classes-raw-javascript/
 var myClassAdmin = {
-  hasClass: function(element, className) {
+  hasClass: function(element, myClassName) {
     if (element.classList) {
-      return element.classList.contains(className);
+      return element.classList.contains(myClassName);
     } else {
-        return !!element.className.match(new RegExp('\\b' + className + '\\b'));
+        return !!element.className.match(new RegExp('\\b' + myClassName + '\\b'));
     }
   },
-  addClass: function(element, className) {
+  addClass: function(element, myClassName) {
     if (element.classList) {
-      element.classList.add(className);
-    } else if (!hasClass(element, className)) {
-        element.className += " " + className;
+      element.classList.add(myClassName);
+    } else if (!hasClass(element, myClassName)) {
+        element.className += " " + myClassName;
       }
   },
-  removeClass: function(element, className) {
+  removeClass: function(element, myClassName) {
     if (element.classList) {
-      element.classList.remove(className);
-    } else if (hasClass(element, className)) {
-        var reg = new RegExp('\\b' + className + '\\b');
+      element.classList.remove(myClassName);
+    } else if (hasClass(element, myClassName)) {
+        var reg = new RegExp('\\b' + myClassName + '\\b');
         element.className=element.className.replace(reg, ' ');
+    }
+  },
+  replaceClass: function(element, myOldClass, myNewClass) {
+    if (element.classList) {
+      element.classList.remove(myOldClass);
+      element.classList.add(myNewClass);
+    } else {
+      element.className.replace(myOldClass, myNewClass);
     }
   }
 };
