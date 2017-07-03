0b940038a80ae14e1cde73d6df03b7b6d7225d7c
Raniro Coelho <errecme@gmail.com>
2017-07-03 20:49:16 -0300

    Adjust peripherals hero position and a scale

--- js/section-games.js
+++ js/section-games.js
@@ -1,11 +1,11 @@
 var changeBg = (function() {
   var bg = document.querySelector('.games__bg-characters');
-  var bgArr = ['1', '2', '3', '4', '5'];
+  var bgArr = ['1', '2', '3', '4', '5', '6', '7', '8'];
   var index = 0;
 
   setInterval(_changeBg, 5000);
 
   function _changeBg() {
-    myClassAdmin.replaceClass(bg, 'games__bg-characters--' + bgArr[index % 5],'games__bg-characters--' + bgArr[++index % 5]);
+    myClassAdmin.replaceClass(bg, 'games__bg-characters--' + bgArr[index % 8],'games__bg-characters--' + bgArr[++index % 8]);
   }
 })();
