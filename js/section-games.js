a0618491af606afba9909135a1bfb11d37932cdb
Raniro Coelho <errecme@gmail.com>
2017-07-07 16:45:52 -0300

    Refactor games section bg swtich code, adjust roll bg speed

--- js/section-games.js
+++ js/section-games.js
@@ -1,11 +1,17 @@
 var changeBg = (function() {
   var bg = document.querySelector('.games__bg-characters');
-  var bgArr = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  var bgChildrenCount = bg.children.length;
+  var bgArr = [];
   var index = 0;
+
+  for(var i = bgChildrenCount - 1; i >= 0; i--) {
+    bgArr[i] = i;
+  }
 
   setInterval(_changeBg, 5000);
 
   function _changeBg() {
-    myClassAdmin.replaceClass(bg, 'games__bg-characters--' + bgArr[index % 8],'games__bg-characters--' + bgArr[++index % 8]);
+    myClassAdmin.removeClass(bg.children[index % bgChildrenCount], 'games__bg-character--show');
+    myClassAdmin.addClass(bg.children[++index % bgChildrenCount], 'games__bg-character--show');
   }
 })();
