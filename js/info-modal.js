f9cdc5b3527b0dd69360353feb132f35f602610e
Raniro Coelho <errecme@gmail.com>
2017-07-11 21:45:44 -0300

    Add showInfoModal module

---
+++ js/info-modal.js
@@ -0,0 +1,19 @@
+var showInfoModal = (function() {
+  var infoModal  = document.querySelector('.info-modal');
+  var closeModal = document.querySelector('.info-modal__close');
+
+  var modifierShowClass = 'info-modal--show';
+  var modifierHideClass = 'info-modal--hide';
+
+  closeModal.addEventListener('click', function() {
+    myClassAdmin.addClass(infoModal, modifierHideClass);
+  });
+  window.addEventListener('scroll', _checkPosition);
+
+  function _checkPosition() {
+    if (window.scrollY > 200) {
+      myClassAdmin.addClass(infoModal, modifierShowClass);
+      window.removeEventListener('scroll', _checkPosition);
+    }
+  }
+})();
